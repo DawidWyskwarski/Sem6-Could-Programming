04267bd44c9d5f39c59aa89c682854ffdb4c35b4
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2026-06-03 23:38:55 +0200

    list 7 - docker swarm & terraform adjustments

--- song-release-system/terraform/infra/outputs.tf
+++ song-release-system/terraform/infra/outputs.tf
@@ -1,17 +1,10 @@
-output "rds_endpoint" {
-  value = aws_db_instance.main.address
+output "rds_hostname" {
+  value       = aws_db_instance.main.address
+  description = "The public endpoint of your RDS database"
 }
 
-resource "aws_ssm_parameter" "db_url" {
-  for_each = toset(var.services)
-  name     = "/microservices/${each.key}/db_url"
-  type     = "SecureString"
-  value    = "postgresql://${each.key}_user:${random_password.db_passwords[each.key].result}@${aws_db_instance.main.address}:5432/${each.key}"
-}
-
-resource "aws_ssm_parameter" "db_password" {
-  for_each = toset(var.services)
-  name     = "/microservices/${each.key}/db_password"
-  type     = "SecureString"
-  value    = random_password.db_passwords[each.key].result
+output "service_db_passwords" {
+  value     = { for k, v in random_password.db_passwords : k => v.result }
+  sensitive = true
+  description = "Map of microservice names to their generated DB passwords"
 }
